7976005516deae28125ca8d4917b294fdb33e5d5
Can Günyel <cangunyel@gmail.com>
2024-11-25 16:18:50 +0300

    Forgot to push utils

--- utils/Authentication.py
+++ utils/Authentication.py
@@ -1,5 +1,5 @@
 from passlib.context import CryptContext
-
+from schemas.Authentication import AuthResponse
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
@@ -9,3 +9,11 @@ def get_password_hash(plain_password):
     return pwd_context.hash(plain_password)
 
 
+
+def create_response( user_message: str, error_status: int, error_message: str,token: str = None):
+    return AuthResponse(
+        token=token,
+        user_message=user_message,
+        error_status=error_status,
+        error_message=error_message
+    )
