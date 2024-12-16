9efedf1570d6f7908c84e39f1d562102f099adb1
Can Günyel <cangunyel@gmail.com>
2024-12-16 11:20:06 +0300

    File naming changed for broader scope

--- schemas/PhotoUpload.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-
-class PhotoUploadResponse(BaseModel):
-    photoUrl: Optional[str] = None
-    user_message: str
-    error_status:int
-    system_message: str    
